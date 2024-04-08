669b04f4b812834c3ead4ba0cbb7947b4624191c
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-04-08 00:12:19 -0700

    Add files via upload

--- typevoid.ts
+++ typevoid.ts
@@ -8,3 +8,27 @@ import "./Guess"
 import "./XYZT"
 (async: any) =>{'return,color,Guess,fermat'}
 (await: any) =>{'worldsheet,XYZT,Letter,InventorySlots'}
+interface iteration{
+   string:["fermat + color + Guess"]
+   AsyncIterable: any
+   String:["fermat - color - Guess"]
+   AsyncIterableIterator: any
+   toString:["fermat * color * Guess"]
+   AsyncIterator: any
+}
+namespace NodeIterator{
+    number:["fermat % color % Guess"]
+    AsyncGenerator:
+    Number:["fermat / color / Guess"]
+    AsyncGeneratorFunction:
+    tonumber:["fermat | color | Guess"]
+    AsyncGeneratorFunctionConstructor:
+    NodeIterator
+}
+void function(await: any){
+   void ['XYZT + Letter + InventorySlots']
+   void ['XYZT - Letter - InventorySlots']
+   void ['XYZT * Letter * InventorySlots']
+   void ['XYZT % Letter % InvrntorySlots']
+   return await
+} 
