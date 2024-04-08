9f21a2b2a3293271a91a58ebb72454451360c392
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-04-08 10:32:18 +0800

    Add files via upload

---
+++ typevoidWebStorm.ts
@@ -0,0 +1,10 @@
+import "./InventorySlots"
+import "./worldsheet"
+import "./return"
+import "./Letter"
+import "./fermat"
+import "./color"
+import "./Guess"
+import "./XYZT"
+void async function (){'XYZT,color,Guess,fermat'}
+void await function (){'worldsheet,return,Letter,InventorySlots'}
